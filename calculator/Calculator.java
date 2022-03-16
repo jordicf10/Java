99305c9b3be4c5951a8628f7675c57c561005b6d
= <=>
2022-03-16 20:19:27 +0100

    Second update Calculator with getters, setters, try catch and methods

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -11,19 +11,38 @@ public class Calculator{
         this.num2 = num2;
     }
 
+    public int getNum1(){
+        return num1;
+    }
+
+    public void setNum1(int num1){
+        this.num1 = num1;
+    }
+
+    public int getNum2(){
+        return num2;
+    }
+
+    public void setNum2(int num2){
+        this.num2 = num2;
+    }
+
     public static void main(String[] args){
 
         int num1 = 0;
         int num2 = 0;
         boolean error = true;
+        Calculator myCalculator = new Calculator(num1,num2);
         do{
             try{
                 System.out.println("Introduce your first number:");
                 Scanner number1 = new Scanner(System.in);
                 num1 = number1.nextInt();
+                myCalculator.setNum1(num1);
                 System.out.println("Introduce your second number:");
                 Scanner number2 = new Scanner(System.in);
                 num2 = number2.nextInt();
+                myCalculator.setNum2(num2);
                 error = false;
                 }catch(Exception e){
                     System.out.println("Please, introduce a correct number");
@@ -31,7 +50,7 @@ public class Calculator{
                 }
         }while(error);
 
-        Calculator myCalculator = new Calculator(num1,num2);
+        
         int option = 0;
  
 do{
@@ -50,19 +69,19 @@ do{
 
                     switch(option){
                         case 1:
-                     System.out.println(num1 + " + " + num2 + " = " + myCalculator.add(num1,num2));
+                     System.out.println(myCalculator.getNum1() + " + " + myCalculator.getNum2() + " = " + myCalculator.add(num1,num2));
                      break;
                     case 2:
-                     System.out.println(num1 + " - " + num2 + " = " + myCalculator.substract(num1,num2));
+                     System.out.println(myCalculator.getNum1() + " - " + myCalculator.getNum2() + " = " + myCalculator.substract(num1,num2));
                      break;
                     case 3:
-                     System.out.println(num1 + " * " + num2 + " = " + myCalculator.multiply(num1,num2));
+                     System.out.println(myCalculator.getNum1() + " * " + myCalculator.getNum2() + " = " + myCalculator.multiply(num1,num2));
                      break;
                     case 4:
-                     System.out.println(num1 + " / " + num2 + " = " + myCalculator.divide(num1,num2));
+                     System.out.println(myCalculator.getNum1() + " / " + myCalculator.getNum2() + " = " + myCalculator.divide(num1,num2));
                      break;
                     case 5:
-                     System.out.println(num1 + " % " + num2 + " = " + myCalculator.modulo(num1,num2));
+                     System.out.println(myCalculator.getNum1() + " % " + myCalculator.getNum2() + " = " + myCalculator.modulo(num1,num2));
                      break;
                     }
  
